8e1c0493231dc2a559ec666010cc5b62040f4c13
foxofarctic <lae1@williams.edu>
2019-08-01 17:50:58 -0400

    got the map working, also did analysis of east vs west on stations and removed some data I found suspect -- load fixedData for that

--- ice_coeff_analysis.m
+++ ice_coeff_analysis.m
@@ -1,25 +1,87 @@
 % % Tsai eq -- Ice Coefficient analysis 
 clear;close all;
 load('/media/lucas/Elements/IRIS_Sea_Ice/matlab/iceData/TsaiCoeffs.mat');
-figure(1);clf;hold on
-plot([TsaiCoeffs.DistancefromCoastkm],[TsaiCoeffs.IceCoefficientshortshort],'o', 'MarkerFaceColor','b');
-plot([TsaiCoeffs.DistancefromCoastkm],zeros(length([TsaiCoeffs.DistancefromCoastkm])),'k');
+
+figure(2);clf;hold on
+x = [TsaiCoeffs.DistancefromCoastkm]';
+y = [TsaiCoeffs.IceCoefficientshortshort]';
+plot(x,y,'o');
 title('Ice Coefficient Effect vs Distance from Coast -- 52 Stations');
-xlabel('Distance from Coast');
+xlabel('Distance from Coast (km)');
 ylabel('Tsai Ice Coefficients');
 
-%%
+%linear regression
+format long;
+b1 = x\y;
+X = [ones(length(x),1) x];
+b = X\y;
+yCalc2 = X*b;
+plot(x,yCalc2); % regression line
+cor = corrcoef(y,yCalc2);
+xl = xlim;
+yl =ylim;
+zline = refline(0,0) % zeroline
+zline.Color = 'k'
+text(xl(2)-102, yl(2)-.02,sprintf('corrcoeff: %.6f',cor(1,2)))
+text(xl(2)-125, yl(2)-.01,sprintf('y = %.3f + %.6fx',b(1),b(2)))
+legend('station','linear regression','Location','northwest');
+figure(1); clf;
+geoscatter([TsaiCoeffs.Latitude],[TsaiCoeffs.Longitude],'filled','SizeData',100)
+geobasemap('landcover')
+legend('Stations Used for Distance Analysis')
+%% graphs of different transects -- west east
+clear; close all;
+% load('/media/lucas/Elements/IRIS_Sea_Ice/matlab/iceData/TsaiCoeffs.mat');
+load('/media/lucas/Elements/IRIS_Sea_Ice/matlab/iceData/TsaiCoeffsFixed.mat');
+% TsaiCoeffs = table2struct(TsaiCoefficients16px);
+transectGroup = {["18","19","20","21","22"], ["23","24","24","25","26","27"]};
+names = ["Western","Eastern"];
 
-ax = worldmap('USA');
-can = load('korea');
-Z= can.map;
-% states = shaperead('usastatelo', 'UseGeoCoords', true);
-% faceColors = makesymbolspec('Polygon',{'INDEX', [1 numel(states)], 'FaceColor',polcmap(numel(states))}); % NOTE - colors are random
-% geoshow(ax, states, 'DisplayType', 'polygon','SymbolSpec', faceColors)
-R = georasterref('RasterSize',size(Z),'Latlim',[60 75], 'Lonlim', [145 170]);
-figure; 
-worldmap(Z,R)
-geoshow(Z,R,'DisplayType','surface','ZData',zeros(size(Z)),'CData',Z)
-demcmap(Z)
-% geoshow(ax, coastlat, coastlon,'DisplayType', 'polygon', 'FaceColor', [.45 .60 .30])
+cont = contains([TsaiCoeffs.Station], transectGroup{1});
+tind = {find(cont == 1) find(cont == 0)};
+figure(2);clf; hold on
 
+for i = 1:length(tind)
+    x = [TsaiCoeffs(tind{i}).DistancefromCoastkm]';
+    y = [TsaiCoeffs(tind{i}).IceCoefficientshortshort]';
+    subplot(2,1,i); hold on
+    plot(x,y,'o');
+    
+    
+    format long;
+    b1 = x\y;
+    X = [ones(length(x),1) x];
+    b = X\y;
+    yCalc2 = X*b;
+    plot(x,yCalc2); % regression line
+    cor = corrcoef(y,yCalc2);
+    
+    zline = refline(0,0) % zeroline
+    zline.Color = 'k'
+    ylim([-.3 0.1]);
+    yl = ylim;
+    xl = xlim;
+    text(xl(2)-102, yl(1)+.05,sprintf('corrcoeff: %.6f',cor(1,2)))
+    text(xl(2)-115, yl(1)+.04,sprintf('y = %.3f + %.6fx',b(1),b(2)))
+    title(sprintf('Stations on %s Alaska Transects',names(i)))
+    ylabel('Tsai Ice Coefficient');
+end
+xlabel('Distance from Coast (km)');
+legend('station','linear regression','Location','northwest');
+
+
+figure;
+region = categorical(cont);
+gb = geobubble([TsaiCoeffs.Latitude],[TsaiCoeffs.Longitude],[],region,'BubbleWidthRange',20,'Basemap','bluegreen','LegendVisible','off');
+% legend('West', 'East')
+% gb.Title = 'Alaskan Map of East/West Stations';
+
+
+% Make alaska Map
+figure(1); clf;
+geoscatter([TsaiCoeffs(tind{1}).Latitude],[TsaiCoeffs(tind{1}).Longitude],'filled','SizeData',100)
+hold on;
+geoscatter([TsaiCoeffs(tind{2}).Latitude],[TsaiCoeffs(tind{2}).Longitude],'filled','SizeData',100)
+hold off;
+geobasemap('landcover')
+legend('Western Stations','Eastern Stations')
